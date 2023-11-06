085c762ca253cd3286da92fea41415f43b0cdf7b
Sergio RA <Sergrojas29@gmail.com>
2023-11-05 21:07:49 -0500

    inti folder set up

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx'
+import Home from './pages/Home'
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      // {
+      //   path: 'project',
+      //   element: <ProjectPage />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
